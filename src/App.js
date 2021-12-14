e2355fe5519d05507ee134cd81530ff3ec887df1
Krish <krish0941.cse19@chitkara.edu.in>
2021-12-15 00:42:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React from 'react'
-import HEADSECTION from './Components/Headsection/head'
-import FEATURES from './Components/Features/features'
-import RESTAURANTS from './Components/Restaurants/restaurants'
-import FOOTER from './Components/Footersection/footer'
 import './App.css';
+import Header from './components/Header';
 
-// Made by Krish Goyal
+import { useState } from 'react';
+import FeatureSection from './components/FeatureSection';
+import PocketSection from './components/PocketSection';
+import CitySection from './components/CitySection.js';
+import ActionSection from './components/ActionSection';
+import Footer from './components/Footer';
+import Slider from './components/Slider';
+import SignIn from './components/SignIn';
+import Login from './components/Login';
+// import LogIn from './components/LogIn';
 
-function App() {
-  return (
-    <div className="body">
-      <HEADSECTION />
-      <FEATURES />
-      <RESTAURANTS />
-      <FOOTER />
-    </div>
-  )
+function App (){
+
+   
+
+ 
+        return (
+            <div className="App">
+            <div className="header123"><Header/></div>
+           
+<div className="brightlow">
+            <div className="feature123"><FeatureSection/></div>
+            <div className="pocket123"> <PocketSection/></div>
+            <div className="action123"> <ActionSection/></div>
+            
+            <div className="city123"><CitySection/></div>
+            <div className="footer123"><Footer/></div>
+            </div></div>
+        
+    );
 }
 
 export default App;
